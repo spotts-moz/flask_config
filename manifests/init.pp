612f4625e6e39aebd2dbee39e7fd312397c58b7c
root <root@ip-172-31-60-213.ec2.internal>
2016-06-08 06:56:08 +0000

    Fixed flask app path URL

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,7 +13,7 @@ node default {
     include git
     include cron
     class { 'Git::Clone':
-             repo => "https://github.com/spotts-moz/flaskt_app",
+             repo => "https://github.com/spotts-moz/flask_app",
              dpath => "/var/www/${hostname}.${domain}/src",
            }
 
