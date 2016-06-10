13f0515c9765d301a264743765e9202108ca6f0b
spotts-moz <spotts61@gmail.com>
2016-06-09 18:38:33 -0700

    Moving clone from class to definition

--- modules/git/manifests/init.pp
+++ modules/git/manifests/init.pp
@@ -1,3 +1,23 @@
-    class git {
+class git {
+      package { 'git':
+      ensure => installed,
+      
+  define clone ($repo, $dpath) {
+  
+    file { "${dpath}" :
+      ensure  => directory,
+      mode    => 0755,
     }
 
+
+    vcsrepo { "${dpath}":
+      ensure   => latest,
+      provider => git,
+      require  => [ Package["git"] ],
+      source   => "${repo}.git",
+      revision => 'master',
+    }
+
+  }
+
+}
